28250400f94360adf7b9fb712aff683d96a69426
Anna Jones <annamariajones@live.co.uk>
2025-03-02 18:55:32 +0000

    register,login css

--- react-product-filters/src/App.jsx
+++ react-product-filters/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
+import NavMenu from "./components/Nav/nav";
 
 function App() {
   return (
     <>
       <main>
+        <NavMenu />
         <Routes>
           <Route path="/" element={<div>Home Page</div>} />
           <Route path="/register" element={<Register />} />
